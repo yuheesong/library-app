d5f282b98e07d73f35bcc32ca6ac513f18cbdda0
yuheesong <thddbgml345@naver.com>
2023-05-20 18:38:32 +0900

    스프링 컨테이너 사용

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,7 +4,6 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import com.group.libraryapp.service.user.UserService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,8 +12,8 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    public UserController(JdbcTemplate jdbcTemplate){
-        this.userService = new UserService(jdbcTemplate);
+    public UserController(UserService userService){
+        this.userService = userService;
     }
 
     @PostMapping("/user")
