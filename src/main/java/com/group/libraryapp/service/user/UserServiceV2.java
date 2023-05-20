c8459e2d92159b4b8ec4fcfe3a8a6285b305fdaa
yuheesong <thddbgml345@naver.com>
2023-05-20 21:19:53 +0900

    트랜잭션 적용

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,23 +18,25 @@ public class UserServiceV2 {
     public UserServiceV2(UserRepository userRepository){
         this.userRepository = userRepository;
     }
+    @Transactional
     public void saveUser(UserCreateRequest request){
-        User u = userRepository.save(new User(request.getName(), request.getAge()));
+        userRepository.save(new User(request.getName(), request.getAge()));
     }
+
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers(){
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
-
+    @Transactional
     public void updateUser(UserUpdateRequest request){
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUser(String name){
         User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
 
